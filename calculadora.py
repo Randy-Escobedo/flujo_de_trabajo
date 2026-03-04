ff0687991a638c92bfcf44df44371776d213e4db
Randy Brandon Juan Escobedo <randyescobedo53@gmail.com>
2026-03-03 23:38:43 -0600

    Raiz cuadrada

--- calculadora.py
+++ calculadora.py
@@ -1,1 +1,7 @@
-# Aqui van las funciones de la calculadora
+# Raiz cuadrada
+import math
+
+def sqrt(x):
+    if x < 0:
+        raise ValueError("No se puede sacar raiz de un numero negativo")
+    return math.sqrt(x)
